77d1b0bdfc9a4f23f92cf74ff1369ea4e03eb925
Bharath21021995 <bharathav35@gmail.com>
2021-12-22 17:09:34 +0530

    B added

--- src/com/sample/GitClass2.java
+++ src/com/sample/GitClass2.java
@@ -1,5 +1,19 @@
 package com.sample;
 
 public class GitClass2 {
+	
+	private void sysout() {
+		// TODO Auto-generated method stub
 
+	}
+	
+	private void sysout1() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void sysyt() {
+		// TODO Auto-generated method stub
+
+	}
 }
